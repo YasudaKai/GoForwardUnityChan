595d19346309c8ee40f7981f1c5ce1abbfb0e015
kai.yasuda <kai0603603@gmail.com>
2020-10-06 17:56:05 +0900

    Initial commit

---
+++ Assets/CubeSoundEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSoundEffect : MonoBehaviour
+{
+    private GameObject ground;
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "GroundTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        if(other.gameObject.tag == "CubeTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
